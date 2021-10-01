9446bc09abc8f42a1769cf00393739e2ff11dce3
VAB1998 <blanco.vincenzo@outlook.com>
2021-10-01 15:59:31 +0200

    Made an AXIOS request using parameters

--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -5,13 +5,28 @@
 </template>
 
 <script>
-
+import axios from 'axios'
 
 export default {
     name: 'Movies',
     components: {
         
-    }
+    },
+
+    created : function() {
+        //Make HTTP  GET Request to an API
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=2c9b181fd830bd18b14d45907ca913b7',
+        {
+            params: {
+                query : 'fight'
+            }
+        })
+        .then((object) =>{
+            console.log(object.data.results)
+            
+        });
+    },
+
 }
 </script>
 
